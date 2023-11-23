50e8704648f7ef400137be32dcd8ad5975e0d165
Lexsak <bartoszpiprek123@gmail.com>
2023-11-23 13:08:57 +0100

    featured and latest posts

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PostsService } from 'src/app/services/posts.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  featuredPostsArray: any;
+  latestPostsArray: any;
 
+  constructor(private postService: PostsService) {}
+
+  ngOnInit(): void {
+    this.postService.loadFeatured().subscribe((val) => {
+      this.featuredPostsArray = val;
+    });
+    this.postService.loadLatest().subscribe(val => {
+      this.latestPostsArray = val
+    })
+  }
 }
